ad5814327e4f0bb042ae42a1f748957f4b069541
armughanahmed <armughan.ahmed@yahoo.com>
2024-12-07 03:12:04 +0100

    domain implementation added

--- src/main/java/com/armughanahmed/ecommerce/ecommerce/domain/Category.java
+++ src/main/java/com/armughanahmed/ecommerce/ecommerce/domain/Category.java
@@ -1,4 +1,19 @@
 package com.armughanahmed.ecommerce.ecommerce.domain;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Product> products;
 }
--- src/main/java/com/armughanahmed/ecommerce/ecommerce/domain/Product.java
+++ src/main/java/com/armughanahmed/ecommerce/ecommerce/domain/Product.java
@@ -1,4 +1,29 @@
 package com.armughanahmed.ecommerce.ecommerce.domain;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @Column(nullable = false)
+    private Integer stock;
 }
